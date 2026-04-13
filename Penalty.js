65a0ff50e668cfa3cfd4d2c2f9d25f63b3b06c0e
mansigaur77-crypto <mansigaur77@gmail.com>
2026-04-13 20:37:50 +0530

    Delete Penalty.js

--- Penalty.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const penaltySchema = new mongoose.Schema({
-  violation: String,
-  state: String,
-  fine_amount: Number
-});
-
-module.exports = mongoose.models.Penalty || mongoose.model("Penalty", penaltySchema);
